d46d43dd6e4462fa3c2d6e0248727c81d213050a
Jpendy <Jpendy256@gmail.com>
2020-07-29 10:53:03 -0700

    commit

--- src/components/pageButtons/PageButtons.jsx
+++ src/components/pageButtons/PageButtons.jsx
@@ -5,8 +5,9 @@ import styles from './PageButtons.css';
 const PageButtons = ({ page, handleClick }) => (
   
   <section className={styles.buttonSection} >
-    {page > 1 && <button name='previous' onClick={handleClick} >Previous</button>}
-    {page < 25 && <button name='next' onClick={handleClick}>Next</button>}
+    <button disabled={page === 1} name='previous' onClick={handleClick} >Previous</button>
+    <span>{page}/25</span>
+    <button disabled={page === 25} name='next' onClick={handleClick}>Next</button>
   </section>
   
 );
