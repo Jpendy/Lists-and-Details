477850bc861bf752267adad82cbd27f662981e8d
Jpendy <Jpendy256@gmail.com>
2020-07-29 11:49:29 -0700

    added search params pagination

--- src/hooks/pageSetupHook.js
+++ src/hooks/pageSetupHook.js
@@ -1,11 +1,20 @@
 import { useState, useEffect } from 'react';
 import { apiFetch } from '../services/apiFetch';
+import { useLocation, useHistory } from 'react-router-dom';
 
 export const usePageSetup = () => {
   const [listArray, setListArray] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [page, setPage] = useState(1);
+  // const [page, setPage] = useState(1);
+  const history = useHistory();
+  const location = useLocation();
+  const page = Number((new URLSearchParams(location.search)).get('page') || 1);
  
+  const handleClick = ({ target }) => {
+    if(target.name === 'next') history.push(`?page=${page + 1}`);   //  setPage(page => page + 1);
+    if(target.name === 'previous') history.push(`?page=${page - 1}`);   // setPage(page => page - 1);
+  };
+
   useEffect(() => {
     setLoading(true);
     apiFetch(page)
@@ -13,10 +22,7 @@ export const usePageSetup = () => {
       .finally(() => setLoading(false));
   }, [page]);
 
-  const handleClick = ({ target }) => {
-    if(target.name === 'next') setPage(page => page + 1);
-    if(target.name === 'previous') setPage(page => page - 1);
-  };
+  
 
   return {
     listArray,
--- src/services/apiFetch.js
+++ src/services/apiFetch.js
@@ -1,4 +1,4 @@
-export const apiFetch = page => {
+export const apiFetch = (page = 1) => {
   return fetch(`https://last-airbender-api.herokuapp.com/api/v1/characters?page=${page}`)
     .then(res => res.json());
 };
