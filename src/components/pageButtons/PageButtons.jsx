35342819362e40a61df8be0dab8bd2c45d0fdfba
Jpendy <Jpendy256@gmail.com>
2020-07-28 17:38:02 -0700

    commit

--- src/components/pageButtons/PageButtons.jsx
+++ src/components/pageButtons/PageButtons.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './PageButtons.css';
 
 const PageButtons = ({ page, handleClick }) => (
-  <>
-    {page < 25 && <button name='next' onClick={handleClick}>Next</button>}
+  
+  <section className={styles.buttonSection} >
     {page > 1 && <button name='previous' onClick={handleClick} >Previous</button>}
-  </>
+    {page < 25 && <button name='next' onClick={handleClick}>Next</button>}
+  </section>
+  
 );
 
 PageButtons.propTypes = {
